4a966fa0be15e87ebb8b10c6df4191c5146225b5
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 21:20:34 +0900

    Add Spanish weekday strings from Bank 00

--- src/home/text/locales/es.asm
+++ src/home/text/locales/es.asm
@@ -14,3 +14,12 @@ String_Space::    db " @"
 PlaceJPRouteText::
 PlaceWatashiText::
 PlaceKokoWaText:: db "-<LF>@"
+
+WeekdaySunday::    db "DOMINGO@"
+WeekdayMonday::    db "LUNES@"
+WeekdayTuesday::   db "MARTES@"
+WeekdayWednesday:: db "MI", $c7, "RCOLES@" ; accented glyph, exact ROM byte
+WeekdayThursday::  db "JUEVES@"
+WeekdayFriday::    db "VIERNES@"
+WeekdaySaturday::  db "S", $bf, "BADO@" ; accented glyph, exact ROM byte
+WeekdaySuffix::    db "@"

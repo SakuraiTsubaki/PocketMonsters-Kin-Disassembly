12e5b303899d842ca68ff25f4737dc942264bca9
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 19:48:02 +0900

    Add Italian Bank 00 embedded text strings

---
+++ src/home/text/locales/it.asm
@@ -0,0 +1,16 @@
+; Italian Bank 00 embedded text-engine strings.
+
+TMCharText::      db "T@"
+TrainerCharText:: db "ALLEN.@"
+PCCharText::      db "PC@"
+RocketCharText::  db "ROCKET@"
+PlacePOKeText::   db "POKé@"
+KougekiText::     db "こうげき@"
+SixDotsCharText:: db "……@"
+EnemyText::       db " nemico@"
+PlacePKMNText::   db "<PK><MN>@"
+PlacePOKEText::   db "<PO><KE>@"
+String_Space::    db " @"
+PlaceJPRouteText::
+PlaceWatashiText::
+PlaceKokoWaText:: db "-<LF>@"

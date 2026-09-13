c2f8fa1ad46c99bce1371465a7cffa0a68c57b2b
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 21:34:03 +0900

    Split German dummied text from shared aliases

--- src/home/text/locales/de.asm
+++ src/home/text/locales/de.asm
@@ -11,9 +11,10 @@ EnemyText::       db "Gegn. @"
 PlacePKMNText::   db "<PK><MN>@"
 PlacePOKEText::   db "<PO><KE>@"
 String_Space::    db " @"
+DummiedHyphenText:: db "-<LF>@"
 PlaceJPRouteText::
 PlaceWatashiText::
-PlaceKokoWaText:: db "-<LF>@"
+PlaceKokoWaText:: db "@"
 
 WeekdaySunday::    db "SONNTAG@"
 WeekdayMonday::    db "MONTAG@"

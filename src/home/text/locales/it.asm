a944acbeec008e550e40edf2078117ed9f806e89
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 21:20:26 +0900

    Add Italian weekday strings from Bank 00

--- src/home/text/locales/it.asm
+++ src/home/text/locales/it.asm
@@ -14,3 +14,12 @@ String_Space::    db " @"
 PlaceJPRouteText::
 PlaceWatashiText::
 PlaceKokoWaText:: db "-<LF>@"
+
+WeekdaySunday::    db "DOMENICA@"
+WeekdayMonday::    db "LUNED", $c8, "@" ; final accented glyph, exact ROM byte
+WeekdayTuesday::   db "MARTED", $c8, "@"
+WeekdayWednesday:: db "MERCOLED", $c8, "@"
+WeekdayThursday::  db "GIOVED", $c8, "@"
+WeekdayFriday::    db "VENERD", $c8, "@"
+WeekdaySaturday::  db "SABATO@"
+WeekdaySuffix::    db "@"

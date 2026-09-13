5c0c7dcee48385cce5fcebad48a96e89d5c41d72
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 19:47:54 +0900

    Add French Bank 00 embedded text strings

---
+++ src/home/text/locales/fr.asm
@@ -0,0 +1,16 @@
+; French Bank 00 embedded text-engine strings.
+
+TMCharText::      db "T@"
+TrainerCharText:: db "DRESSEUR@"
+PCCharText::      db "PC@"
+RocketCharText::  db "ROCKET@"
+PlacePOKeText::   db "POKé@"
+KougekiText::     db "こうげき@"
+SixDotsCharText:: db "……@"
+EnemyText::       db " ennemi@"
+PlacePKMNText::   db "<PK><MN>@"
+PlacePOKEText::   db "<PO><KE>@"
+String_Space::    db " @"
+PlaceJPRouteText::
+PlaceWatashiText::
+PlaceKokoWaText:: db "@"

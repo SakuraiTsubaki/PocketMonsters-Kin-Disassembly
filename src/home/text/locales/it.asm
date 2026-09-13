876af980fcc1c7e459537481eb2764292e700fde
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 21:34:12 +0900

    Split Italian dummied text from shared aliases

--- src/home/text/locales/it.asm
+++ src/home/text/locales/it.asm
@@ -11,9 +11,10 @@ EnemyText::       db " nemico@"
 PlacePKMNText::   db "<PK><MN>@"
 PlacePOKEText::   db "<PO><KE>@"
 String_Space::    db " @"
+DummiedHyphenText:: db "-<LF>@"
 PlaceJPRouteText::
 PlaceWatashiText::
-PlaceKokoWaText:: db "-<LF>@"
+PlaceKokoWaText:: db "@"
 
 WeekdaySunday::    db "DOMENICA@"
 WeekdayMonday::    db "LUNED", $c8, "@" ; final accented glyph, exact ROM byte

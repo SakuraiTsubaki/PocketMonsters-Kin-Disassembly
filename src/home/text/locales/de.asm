c9dc6deca7884d613abee33545c96f44365e3fda
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 19:47:49 +0900

    Add German Bank 00 embedded text strings

---
+++ src/home/text/locales/de.asm
@@ -0,0 +1,16 @@
+; German Bank 00 embedded text-engine strings.
+
+TMCharText::      db "TM@"
+TrainerCharText:: db "TRAINER@"
+PCCharText::      db "PC@"
+RocketCharText::  db "ROCKET@"
+PlacePOKeText::   db "POKé@"
+KougekiText::     db "こうげき@"
+SixDotsCharText:: db "……@"
+EnemyText::       db "Gegn. @"
+PlacePKMNText::   db "<PK><MN>@"
+PlacePOKEText::   db "<PO><KE>@"
+String_Space::    db " @"
+PlaceJPRouteText::
+PlaceWatashiText::
+PlaceKokoWaText:: db "-<LF>@"

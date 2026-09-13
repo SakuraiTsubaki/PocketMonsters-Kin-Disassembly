cbb9615c829479672bc3cbcf1c82018f9e6d78c8
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 19:48:11 +0900

    Add Spanish Bank 00 embedded text strings

---
+++ src/home/text/locales/es.asm
@@ -0,0 +1,16 @@
+; Spanish Bank 00 embedded text-engine strings.
+
+TMCharText::      db "T@"
+TrainerCharText:: db "ENTREN.@"
+PCCharText::      db "PC@"
+RocketCharText::  db "ROCKET@"
+PlacePOKeText::   db "POKé@"
+KougekiText::     db "こうげき@"
+SixDotsCharText:: db "……@"
+EnemyText::       db "Enem. @"
+PlacePKMNText::   db "<PK><MN>@"
+PlacePOKEText::   db "<PO><KE>@"
+String_Space::    db " @"
+PlaceJPRouteText::
+PlaceWatashiText::
+PlaceKokoWaText:: db "-<LF>@"
